a54b1e7f56438bb5ced68ed811ccf29aba179f63
For Sure Rad <hi@forsurerad.com>
2011-08-03 17:30:46 -0400

    keys, nodemon, setup

--- bkln.js
+++ bkln.js
@@ -16,8 +16,21 @@ var querystring = require("querystring");
 var app = require("express").createServer();
 
 app.configure(function(){
-  	app.use(express.static(__dirname + "/public"));
+  	//app.use(express.static(__dirname + "/public"));
   	app.use(express.errorHandler());
+});
+
+app.get("/", function(request, response) {
+	response.send("bkln:index")
+});
+
+app.get("/:key", function(request, response) {
+	var key = request.param("key");
+	console.log("key:"+key);
+	if (key == "g") {
+		var URL = "http://google.com";
+		response.send("", {"Location":URL}, 301);
+	}
 });
 
 app.listen(3001, "127.0.0.1");
