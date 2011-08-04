359ab8c3e3df137a8cb54ec8e2c8f6bf521dd7ee
For Sure Rad <hi@forsurerad.com>
2011-08-04 16:20:26 -0400

    url shortening works great with tracking

--- bkln.js
+++ bkln.js
@@ -21,14 +21,15 @@ var db = new sqlite.Database();
 db.open("./data/bkln.db", function (error) {
   	if (error) {
       	throw error;
-  	};
-  	db.execute("CREATE TABLE IF NOT EXISTS URLs (id INTEGER PRIMARY KEY, key TEXT UNIQUE, URL TEXT, hits INTEGER);",
-    	function (error, rows) {
-        	if (error) {
-        		throw error;
-        	};
-    	}
-    );
+  	} else {
+  		db.execute("CREATE TABLE IF NOT EXISTS URLs (id INTEGER PRIMARY KEY, key TEXT UNIQUE, URL TEXT, hits INTEGER);",
+    		function (error, rows) {
+        		if (error) {
+        			throw error;
+        		};
+    		}
+    	);
+	};
 });
 
 app.configure(function(){
@@ -37,10 +38,12 @@ app.configure(function(){
 });
 
 app.get("/", function(request, response) {
-	response.send("bkln::index");
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write("BKLN.ME");
+	response.end();
 });
 
-app.get("/urls", function(request, response){
+app.get("/urls", function(request, response) {
   	response.writeHead(200, { "Content-Type": "text/html" });
   	db.execute("SELECT * from URLs;",
   		function (error, rows) {
@@ -64,26 +67,38 @@ app.get("/urls", function(request, response){
   	);
 });
 
+function updateHits(key) {
+	var SQL = "UPDATE URLs SET hits=hits+1 WHERE key == '"+key+"';";
+	db.execute(SQL,
+		function (error, rows) {
+			if (error) {
+				throw error;
+			};
+		}
+	);
+};
+
 app.get("/:key", function(request, response) {
 	var key = request.param("key");
-	db.execute("SELECT * from URLs WHERE key == '" + key + "';",
+	db.execute("SELECT * from URLs WHERE key == '"+key+"';",
 		function (error, rows) {
 			if (error) {
 				throw error;
 			} else {
 				if (rows.length === 1) {
 					var URL = rows[0].URL;
-					var hits = rows[0].hits;
-					response.send("", {"Location":URL}, 301);
+					updateHits(key);				
+					response.send("", {"Location":URL}, 302);
 				} else {
 					var URL = "http://bkln.me/";
-					response.send("", {"Location":URL}, 301);
+					updateHits("bkln");
+					response.send("", {"Location":URL}, 302);
 				};
 			};
 		}
 	);
 });
 
-app.listen(3001, "127.0.0.1");
+app.listen(3000, "127.0.0.1");
 
 /* EOF */
