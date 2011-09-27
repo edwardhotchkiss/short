71ec99fe77e1e96c1292fcb135030349667bb38e
Edward Hotchkiss <edward@edwardhotchkiss.com>
2011-09-27 16:03:37 -0400

    as an api or as a service

--- examples/server.js
+++ examples/server.js
@@ -1,47 +1,4 @@
 
-short
-=====
-
-***Node.js Tiny URL generator with analytics***
-
-**Setup**
-
-```bash
-$ npm install short
-```
-***Basic API Usage***
-
-```javascript
-var mongoose = require("mongoose");
-var short = require("short");
-
-mongoose.connect("mongodb://localhost/short");
-
-var URL = "http://nodejs.org/";
-
-short.make(URL, function(error, shortURL) {
-	if (error) {
-		console.error(error);
-	} else {
-		short.get(shortURL.hash, function(error, shortURLObject) {
-			if (error) {
-				console.error(error);
-			} else {
-				var URL = shortURLObject[0].URL
-				var hash = shortURLObject[0].hash;
-				console.log(URL, hash);
-				process.exit(1);
-			};
-		});
-	}
-});
-
-/* EOF */
-```
-
-***As a Server***
-
-```javascript
 var http = require("http");
 var mongoose = require("mongoose");
 var short = require("short");
@@ -79,4 +36,3 @@ app.listen(8080);
 console.log("> Open http://localhost:8080/kQ4c");
 
 /* EOF */
-```
