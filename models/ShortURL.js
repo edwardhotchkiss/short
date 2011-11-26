ee33ef3d4e087080f2711e3779c2fbe0dae0ba3d
Edward Hotchkiss <e@ingk.com>
2011-11-26 11:35:41 -0500

    formating, removed mongoose as a req for the api since its bundled in lib

--- models/ShortURL.js
+++ models/ShortURL.js
@@ -11,11 +11,11 @@ var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
 var shortURL_schema = {
-	id          : ObjectId,
-	URL        	: String,
-	hash        : { type : String },
-	hits        : { type : Number, default: 0 },
-	created_at  : { type : Date, default: Date.now }
+  id          : ObjectId,
+  URL         : String,
+  hash        : { type : String },
+  hits        : { type : Number, default: 0 },
+  created_at  : { type : Date, default: Date.now }
 };
 
 var ShortURLSchema = new Schema(shortURL_schema);
@@ -23,50 +23,50 @@ var ShortURL = mongoose.model("ShortURL", ShortURLSchema);
 
 // return count, if it exists, try another
 ShortURL.checkExists = function(hash, callback) {
-	var query = ShortURL.find({});
-	query.where("hash", hash);
-	query.exec(function(error, shortenedURLS) {
-		if (error) {
-			callback(error, null);
-		} else {
-			callback(null, shortenedURLS);
-		};
-	});
+  var query = ShortURL.find({});
+  query.where("hash", hash);
+  query.exec(function(error, shortenedURLS) {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, shortenedURLS);
+    };
+  });
 };
 
 // search for a URL by its hash
 ShortURL.findByHash = function(hash, callback) {
-	ShortURL.find({ hash: hash }, function(error, URL) {
-		if (error) {
-			callback(error, null);
-		} else {
-			if (URL.length !== 0) {
-				var id = URL[0]._id;
-				ShortURL.updateHitsById(id, function(error) {
-					if (error) {
-						callback(error, null);
-					} else {
-						callback(null, URL);
-					};
-				});
-			} else {
-				callback(null, null);
-			}
-		};
-	}); 
+  ShortURL.find({ hash: hash }, function(error, URL) {
+    if (error) {
+      callback(error, null);
+    } else {
+      if (URL.length !== 0) {
+        var id = URL[0]._id;
+        ShortURL.updateHitsById(id, function(error) {
+          if (error) {
+            callback(error, null);
+          } else {
+            callback(null, URL);
+          };
+        });
+      } else {
+        callback(null, null);
+      }
+    };
+  }); 
 };
 
 // increment hits
 ShortURL.updateHitsById = function(id, callback) {
-	ShortURL.findById(id, function (error, URL) {
-		var hits = URL.hits + 1;
-	    if (!error) {
-		  	URL.hits = hits;
-		  	URL.save(function(error) {
-		    	callback(error);
-		  	});
-		}
-	});
+  ShortURL.findById(id, function (error, URL) {
+    var hits = URL.hits + 1;
+    if (!error) {
+      URL.hits = hits;
+      URL.save(function(error) {
+        callback(error);
+      });
+    }
+  });
 };
 
 module.exports = ShortURL;
