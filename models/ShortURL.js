1066f5cc0fcbce545c1efc68bcb146d5fc8faf6a
Edward Hotchkiss <edward@edwardhotchkiss.com>
2011-12-22 10:50:10 -0500

    More comments, cleaned up code

--- models/ShortURL.js
+++ models/ShortURL.js
@@ -1,27 +1,31 @@
 
 /*!
 
-  Model::ShortURL
+  Mongoose Model:
+    - ShortURL
 
  */
 
-var mongoose = require("mongoose");
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
 
-var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
-
-var shortURL_schema = {
+var ShortURLSchema = new Schema({
   id          : ObjectId,
   URL         : String,
   hash        : { type : String, unique: true },
   hits        : { type : Number, default: 0 },
   created_at  : { type : Date, default: Date.now }
-};
+});
 
-var ShortURLSchema = new Schema(shortURL_schema);
-var ShortURL = mongoose.model("ShortURL", ShortURLSchema);
+var ShortURL = mongoose.model('ShortURL', ShortURLSchema);
 
-// search for a URL by its hash
+/*!
+ @method findByHash
+ @param {String} hash
+ @param {Function} callback
+*/
+
 ShortURL.findByHash = function(hash, callback) {
   ShortURL.find({ hash: hash }, function(error, URL) {
     if (error) {
@@ -43,7 +47,12 @@ ShortURL.findByHash = function(hash, callback) {
   });
 };
 
-// increment hits
+/*!
+ @method updatehitsById
+ @param {ObjectId} id
+ @param {Function} callback
+ */
+
 ShortURL.updateHitsById = function(id, callback) {
   ShortURL.findById(id, function (error, URL) {
     var hits = URL.hits + 1;
