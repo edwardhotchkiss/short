236c80a85578feecc598a2c41b1c2e0945591ea5
Edward Hotchkiss <e@ingk.com>
2011-12-23 02:04:14 -0500

    final

--- examples/bkln.me/app.js
+++ examples/bkln.me/app.js
@@ -20,6 +20,18 @@ var url = require('url'),
 short.connect(MONGO_DB_SHORT);
 
 /*!
+  MongoDB Mongoose on open|error Events
+ */
+
+short.connection.on('open', function(){
+  console.log('mongodb connected');
+});
+
+short.connection.on('error', function(error){
+  throw new Error(error);
+});
+
+/*!
   Setup ExpressJS
  */
 
