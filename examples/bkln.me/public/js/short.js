1699cc81a0ece1f2bd57b6da004e60bbfbba06d5
Edward Hotchkiss <edward@edwardhotchkiss.com>
2011-12-26 17:08:55 -0500

    check for valid urls

--- examples/bkln.me/public/js/short.js
+++ examples/bkln.me/public/js/short.js
@@ -6,18 +6,32 @@
 */
 
 $(document).ready(function() {
-  $('#send').click(function() {
-    $.Notify('Creating Short URL...'); 
-    var postData = {
-      url : $('#url').val()
+  function testValidURL(potentialURL){
+    var URLRegExp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/i;
+    if (URLRegExp.test(potentialURL)){
+      return true;
+    } else {
+      return false;
     };
-    $('#send').val('shortening');
-    $.AJAX('POST', '/api/create', postData, function(response) {
-      setTimeout(function() {
-        $('#url').val(response.url);
-        $.Notify('Shortened URL Created! Click to Copy!');
-      }, 750); 
-    });
+  };
+  $('#send').click(function() {
+    if (testValidURL($('#url').val())) {
+      $.Notify('Creating Short URL...'); 
+      var postData = {
+        url : $('#url').val()
+      };
+      $('#send').val('shortening');
+      $.AJAX('POST', '/api/create', postData, function(response) {
+        setTimeout(function() {
+          $('#send').val('shorten');
+          $('#url').val(response.url);
+          $.Notify('Shortened URL Created! Click to Copy!');
+        }, 750); 
+      });
+    } else {
+      $.Notify('Please Enter a valid URL!');
+      $('#url').val('');
+    };
   });
 });
 
